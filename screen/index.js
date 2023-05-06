15bdafa9539dab218ac5ad13a938b6bb26b0d72b
Stanislav Verman <50764766+Arheil@users.noreply.github.com>
2023-05-06 12:26:23 +0700

    Update index.js

--- screen/index.js
+++ screen/index.js
@@ -1,8 +1,15 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
-	let width = window.innerWidth;
-	let height = window.innerHeight;
+  let width = window.innerWidth;
+  let height = window.innerHeight;
 
-	alert(`Размер экрана: \n ширина ${width}\n высота ${height}`);
+  alert(`Размер экрана: \n ширина ${width}\n высота ${height}`);
+	
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+    const { coords } = position;
+    console.log(coords.latitude, coords.longitude);
+  });
+}
 })
